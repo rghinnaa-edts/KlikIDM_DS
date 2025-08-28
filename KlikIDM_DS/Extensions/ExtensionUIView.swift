7779d74b326b3164da11cc5dcfc3b5439d7ef27c
Rizka Ghinna Auliya <rizka.ghinna@sg-edts.com>
2025-08-28 11:06:29 +0700

    fix: change gradient color

--- KlikIDM_DS/Extensions/ExtensionUIView.swift
+++ KlikIDM_DS/Extensions/ExtensionUIView.swift
@@ -8,27 +8,47 @@
 import UIKit
 
 extension UIView {
-    public func setGradientBackground(_ gradient: UIColor.UIKitGradient, cornerRadius: CGFloat = 0, corners: UIRectCorner) {
+    public func setGradientBackground(_ gradient: [UIColor], cornerRadius: CGFloat = 0, corners: UIRectCorner = .allCorners, borderWidth: CGFloat = 0, borderColor: UIColor? = nil) {
         layer.sublayers?.removeAll { $0 is CAGradientLayer }
+        
+        layer.borderWidth = 0
+        layer.cornerRadius = 0
+        layer.masksToBounds = false
         
         layoutIfNeeded()
         
         let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = gradient.colors.map { $0.cgColor }
-        gradientLayer.startPoint = gradient.startPoint.cgPoint
-        gradientLayer.endPoint = gradient.endPoint.cgPoint
+        gradientLayer.colors = gradient.map { $0.cgColor }
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
         gradientLayer.frame = bounds
         
         if cornerRadius > 0 {
-            let maskLayer = CAShapeLayer()
-            
             let maskPath = UIBezierPath(
                 roundedRect: bounds,
                 byRoundingCorners: corners,
                 cornerRadii: CGSize(width: cornerRadius, height: cornerRadius)
             )
+            
+            let maskLayer = CAShapeLayer()
             maskLayer.path = maskPath.cgPath
             gradientLayer.mask = maskLayer
+            
+            if borderWidth > 0, let borderColor = borderColor {
+                let borderLayer = CAShapeLayer()
+                borderLayer.path = maskPath.cgPath
+                borderLayer.fillColor = UIColor.clear.cgColor
+                borderLayer.strokeColor = borderColor.cgColor
+                borderLayer.lineWidth = borderWidth
+                borderLayer.frame = bounds
+                
+                layer.addSublayer(borderLayer)
+            }
+        } else {
+            if borderWidth > 0 {
+                layer.borderWidth = borderWidth
+                layer.borderColor = borderColor?.cgColor
+            }
         }
         
         layer.insertSublayer(gradientLayer, at: 0)
